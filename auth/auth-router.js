6aaa2a937252c792bfbb8c5a031696c6bb72861c
Noor Al Darraji <077068@yahoo.com>
2019-09-20 12:08:10 -0500

    Downloaded bcrypt and completed the endpoints :hammer:

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,44 @@
 const router = require('express').Router();
+const Users = require('../users/users-model.js')
 
 router.post('/register', (req, res) => {
   // implement registration
+  let { username, password } = req.body
+  const hashedPassword = bcrypt.hashSync(password, 8)
+  Users.add({ username, password: hashedPassword})
+  .then(saved => {
+    res
+    .status(201)
+    .json({ message: `The user ${saved} has been Created succesfully!` })
+  })
+  .catch(()=> {
+    res
+    .status(401)
+    .json({ message: "ERROR: User has not been created!" })
+  })
 });
 
 router.post('/login', (req, res) => {
   // implement login
+  let { username, password } = req.body
+  Users.findBy({ username })
+  .first()
+  .then(user => {
+    if (user && bcrypt.compareSync(password, user.password)) {
+      res
+        .json({ message: "User has been inside the data base"})
+    } else {
+      res
+        .status(401)
+        .json({ message: "You shall not pass!" })
+    }
+  })
+  .catch(error => {
+    res
+        .status(500)
+        .json(error)
+  })
+
 });
 
 module.exports = router;
